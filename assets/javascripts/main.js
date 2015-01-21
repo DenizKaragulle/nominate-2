a01db9488f4fea7a84990a2943dd990bc5571b6d
Chris Mahlke <cmahlke@esri.com>
2015-01-21 13:49:46 -0800

    tmp

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1236,6 +1236,7 @@ require([
 						profileThumbnailListener = on(query(".profileThumbnailUrl"), "click", lang.hitch(this, function (event) {
 							portalUser.getItem(selectedRowID).then(lang.hitch(this, function (userItem) {
 								uploadUserProfileThumbnail(userItem, "PROFILE").then(lang.hitch(this, function (userItem) {
+									console.log("DONE");
 									var user = portal.getPortalUser();
 									console.log(user);
 									console.log(userItem);
