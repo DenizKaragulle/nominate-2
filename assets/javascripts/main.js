8f22d4d7bd8e134e1c161f1290500936e07d6937
Chris Mahlke <cmahlke@esri.com>
2015-01-21 15:36:24 -0800

    tmp

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1234,9 +1234,7 @@ require([
 
 						// update user thumbnail
 						profileThumbnailListener = on(query(".profileThumbnailUrl"), "click", lang.hitch(this, function (event) {
-							uploadUserProfileThumbnail("PROFILE").then(lang.hitch(this, function (evt) {
-								console.log(this);
-							}));
+							uploadUserProfileThumbnail("PROFILE");
 						}));
 					} else {
 						// "SAVE" clicked
@@ -1516,13 +1514,15 @@ require([
 									previewDlg.hide();
 									if (response) {
 										console.log(portalUser);
-										esriRequest({
-											//url:portalUser.userContentUrl,
-											url:lang.replace("{userItemUrl/}", portalUser),
-											handleAs:"json"
-										}).then(function (item) {
-													console.log(item);
-												});
+										portalUser.getItem(selectedRowID).then(function (item) {
+											esriRequest({
+												//url:portalUser.userContentUrl,
+												url:lang.replace("{userItemUrl/}", item),
+												handleAs:"json"
+											}).then(function (item) {
+														console.log(item);
+													});
+										});
 									}
 								}), lang.hitch(this, function (error) {
 									console.warn(error);
