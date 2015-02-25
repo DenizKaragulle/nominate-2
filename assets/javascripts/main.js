414dcbfd6bde876ac5684578ce9d959bf17832ad
Chris Mahlke <cmahlke@esri.com>
2015-02-25 13:05:40 -0800

    tmp

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1837,7 +1837,7 @@ require([
 		}
 
 		function isItemNominated(id) {
-			var queryTask = new QueryTask("http://services1.arcgis.com/4yjifSiIG17X0gW4/arcgis/rest/services/nomcur/FeatureServer/0");
+			var queryTask = new QueryTask(defaults.NOMINATE_ADMIN_FEATURE_SERVICE_URL);
 			var query = new Query();
 			query.returnGeometry = false;
 			query.outFields = ["*"];
